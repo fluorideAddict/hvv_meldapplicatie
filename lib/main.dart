d681eb25ea1acaab73aeb0549a120c7f933c4ee5
josef <joba5039@outlook.com>
2025-12-04 03:57:07 +0100

    home screen progress

--- lib/main.dart
+++ lib/main.dart
@@ -8,22 +8,23 @@ void main() {
   //String ACCESS_TOKEN = const String.fromEnvironment("pk.eyJ1Ijoid2Fnb24tb2YtZG9qaW1hIiwiYSI6ImNtaHZwZ2lrbTA5M2Uya3IyNzlsaHg4YnEifQ.V2UQiWmY-7oneGChjf9Akg");
   MapboxOptions.setAccessToken("pk.eyJ1Ijoid2Fnb24tb2YtZG9qaW1hIiwiYSI6ImNtaHZwZ2lrbTA5M2Uya3IyNzlsaHg4YnEifQ.V2UQiWmY-7oneGChjf9Akg");
 
-  CameraOptions camera = CameraOptions(
-      center: Point(coordinates: Position(-98.0, 39.5)),
-      zoom: 2,
-      bearing: 0,
-      pitch: 0);
+  // CameraOptions camera = CameraOptions(
+  //     center: Point(coordinates: Position(-98.0, 39.5)),
+  //     zoom: 2,
+  //     bearing: 0,
+  //     pitch: 0);
 
-  //runApp(const HartVoorVerkeerApp());
-  runApp(MaterialApp(home: MapWidget(
-    cameraOptions: camera,
-  )));
+  runApp(const HartVoorVerkeerApp());
+  //runApp(MaterialApp(home: MapWidget(
+  //  cameraOptions: camera,
+  //)));
 }
 
 class HartVoorVerkeerApp extends StatelessWidget {
   const HartVoorVerkeerApp({Key? key}) : super(key: key);
 
   @override
+
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Hart voor Verkeer',
@@ -31,9 +32,12 @@ class HartVoorVerkeerApp extends StatelessWidget {
         primaryColor: const Color(0xFF481d39),
         scaffoldBackgroundColor: const Color(0xFFEAE2D5),
       ),
-      home: const StartScreen(),
-      //home: MapWidget(cameraOptions: camera,
+      //home: const StartScreen(),
+
+      // home: MapWidget(
+      //   cameraOptions: camera,
       // ),
+      home: const HomeScreen(),
       debugShowCheckedModeBanner: false,
     );
   }
--- lib/screens/map/home_screen.dart
+++ lib/screens/map/home_screen.dart
@@ -1,4 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:mapbox_maps_flutter/mapbox_maps_flutter.dart';
+
+CameraOptions camera = CameraOptions(
+    center: Point(
+        coordinates: Position(
+          5.2333,
+          52.0906
+        )
+    ),
+    zoom: 12,
+    bearing: 0,
+    pitch: 0
+);
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -11,11 +24,36 @@ class HomeScreen extends StatelessWidget {
         foregroundColor: Colors.white,
         title: const Text('placeholder'),
       ),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(24.0),
-        ),
+      body: MapWidget(
+        cameraOptions: camera,
+        // styleString: MapboxStyles.MAPBOX_STREETS,
+        // initialCameraPosition: CameraPosition(
+        //   target: _initialPosition,
+        //   zoom: 12,
+        // ),
+        // accessToken: "YOUR_MAPBOX_ACCESS_TOKEN_HERE",
+        // onMapCreated: _onMapCreated,
+        // myLocationEnabled: true,
+        // myLocationTrackingMode: MyLocationTrackingMode.Tracking,
+        // rotateGesturesEnabled: true,
+        // tiltGesturesEnabled: true,
+        // compassEnabled: true,
+        // zoomGesturesEnabled: true,
       ),
+
+      // BOTTOM NAV BAR
+      // bottomNavigationBar: BottomNavigationBar(
+      //   currentIndex: _selectedIndex,
+      //   onTap: _onNavTapped,
+      //   items: const [
+      //     BottomNavigationBarItem(
+      //         icon: Icon(Icons.map), label: "Map"),
+      //     BottomNavigationBarItem(
+      //         icon: Icon(Icons.place), label: "Places"),
+      //     BottomNavigationBarItem(
+      //         icon: Icon(Icons.settings), label: "Settings"),
+      //  ],
+      //),
     );
   }
 }
