124bc481d2adfd46e5166a4fd1bb4fcf4efd0037
josef <joba5039@outlook.com>
2025-12-09 07:02:08 +0100

    location tracking functionality implemented

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:hvv_meldapplicatie/screens/map/home_screen.dart';
 import 'package:mapbox_maps_flutter/mapbox_maps_flutter.dart';
 import 'start_screen.dart';
+//import 'package:location/location.dart';
+import 'package:geolocator/geolocator.dart';
 
-void main() {
-  WidgetsFlutterBinding.ensureInitialized();
+void main() async{
   //String ACCESS_TOKEN = const String.fromEnvironment("pk.eyJ1Ijoid2Fnb24tb2YtZG9qaW1hIiwiYSI6ImNtaHZwZ2lrbTA5M2Uya3IyNzlsaHg4YnEifQ.V2UQiWmY-7oneGChjf9Akg");
-  MapboxOptions.setAccessToken("pk.eyJ1Ijoid2Fnb24tb2YtZG9qaW1hIiwiYSI6ImNtaHZwZ2lrbTA5M2Uya3IyNzlsaHg4YnEifQ.V2UQiWmY-7oneGChjf9Akg");
-
-  // CameraOptions camera = CameraOptions(
-  //     center: Point(coordinates: Position(-98.0, 39.5)),
-  //     zoom: 2,
-  //     bearing: 0,
-  //     pitch: 0);
-
+  //sMapboxOptions.setAccessToken("pk.eyJ1Ijoid2Fnb24tb2YtZG9qaW1hIiwiYSI6ImNtaHZwZ2lrbTA5M2Uya3IyNzlsaHg4YnEifQ.V2UQiWmY-7oneGChjf9Akg");
+  await setup();
   runApp(const HartVoorVerkeerApp());
   //runApp(MaterialApp(home: MapWidget(
   //  cameraOptions: camera,
   //)));
+}
+
+Future<void> setup() async {
+  await dotenv.load(fileName: ".env",);
+  MapboxOptions.setAccessToken(dotenv.env["MAPBOX_ACCESS_TOKEN"]!);
 }
 
 class HartVoorVerkeerApp extends StatelessWidget {
--- lib/screens/map/home_screen.dart
+++ lib/screens/map/home_screen.dart
@@ -1,20 +1,33 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
-import 'package:mapbox_maps_flutter/mapbox_maps_flutter.dart';
+import 'package:mapbox_maps_flutter/mapbox_maps_flutter.dart' as mb;
+//import 'package:location/location.dart';
+import 'package:geolocator/geolocator.dart' as gl;
+//import 'package:permission_handler/permission_handler.dart';
 
-CameraOptions camera = CameraOptions(
-    center: Point(
-        coordinates: Position(
-          5.2333,
-          52.0906
-        )
-    ),
-    zoom: 12,
-    bearing: 0,
-    pitch: 0
-);
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  mb.MapboxMap? mapboxMapController;
+
+  StreamSubscription? userPositionStream;
+
+  @override
+  void initState() {
+    super.initState();
+    _determinePosition();
+  }
+  @override
+  void dispose() {
+    userPositionStream?.cancel();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -24,8 +37,9 @@ class HomeScreen extends StatelessWidget {
         foregroundColor: Colors.white,
         title: const Text('placeholder'),
       ),
-      body: MapWidget(
-        cameraOptions: camera,
+      body: mb.MapWidget(
+        //cameraOptions: camera,
+        onMapCreated: _onMapCreated,
       ),
 
       //BOTTOM NAV BAR
@@ -35,8 +49,86 @@ class HomeScreen extends StatelessWidget {
               icon: Icon(Icons.settings), label: "Profile"),
           BottomNavigationBarItem(
               icon: Icon(Icons.settings), label: "Settings"),
-       ],
+        ],
       ),
+    );
+  }
+
+  void _onMapCreated(mb.MapboxMap controller) {
+    setState(() {
+      mapboxMapController = controller;
+    });
+    mapboxMapController?.location.updateSettings(
+      mb.LocationComponentSettings(
+        enabled: true,
+      ),
+    );
+  }
+
+  /// Determine the current position of the device.
+  ///
+  /// When the location services are not enabled or permissions
+  /// are denied the `Future` will return an error.
+  Future<void> _determinePosition() async {
+    bool serviceEnabled;
+    gl.LocationPermission permission;
+
+    // Test if location services are enabled.
+    serviceEnabled = await gl.Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      // Location services are not enabled don't continue
+      // accessing the position and request users of the
+      // App to enable the location services.
+      return Future.error('Location services are disabled.');
+    }
+
+    permission = await gl.Geolocator.checkPermission();
+    if (permission == gl.LocationPermission.denied) {
+      permission = await gl.Geolocator.requestPermission();
+      if (permission == gl.LocationPermission.denied) {
+        // Permissions are denied, next time you could try
+        // requesting permissions again (this is also where
+        // Android's shouldShowRequestPermissionRationale
+        // returned true. According to Android guidelines
+        // your App should show an explanatory UI now.
+        return Future.error('Location permissions are denied');
+      }
+    }
+
+    if (permission == gl.LocationPermission.deniedForever) {
+      // Permissions are denied forever, handle appropriately.
+      return Future.error(
+          'Location permissions are permanently denied, we cannot request permissions.');
+    }
+    gl.LocationSettings locationSettings = gl.LocationSettings(
+      accuracy: gl.LocationAccuracy.high,
+      distanceFilter: 100,
+    );
+    // When we reach here, permissions are granted and we can
+    // continue accessing the position of the device.
+
+    userPositionStream?.cancel();
+    userPositionStream = gl.Geolocator.getPositionStream(
+        locationSettings: locationSettings).listen(
+        (
+        gl.Position? position,
+        ) {
+          if (position != null && mapboxMapController != null) {
+            mapboxMapController?.setCamera(
+              mb.CameraOptions(
+                  center: mb.Point(
+                      coordinates: mb.Position(
+                        position.longitude,
+                        position.latitude,
+                      )
+                  ),
+                  zoom: 15,
+                  bearing: 0,
+                  pitch: 0
+              )
+            );
+          }
+        },
     );
   }
 }
