4729d290ef15cc4f89c3ec3c5ff15c4793a5b031
josef <joba5039@outlook.com>
2025-12-09 18:09:19 +0100

    added code for reading .env and added location permissions to AndroidManifest.xml

--- lib/main.dart
+++ lib/main.dart
@@ -2,16 +2,22 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'screens/start_screen.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:mapbox_maps_flutter/mapbox_maps_flutter.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  
+  await setup();
   runApp(const HartVoorVerkeerApp());
 }
 
+Future<void> setup() async {
+  await dotenv.load(fileName: ".env",);
+  MapboxOptions.setAccessToken(dotenv.env["MAPBOX_ACCESS_TOKEN"]!);
+}
 class HartVoorVerkeerApp extends StatelessWidget {
   const HartVoorVerkeerApp({Key? key}) : super(key: key);
 
